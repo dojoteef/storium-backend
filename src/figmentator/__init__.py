afdafe536f324d74ddb3bb909163d0776a4fad0a
Nader Akoury <dojoteef@gmail.com>
2022-03-14 13:39:41 -0400

    Make sure to intialize the root logger

--- src/figmentator/__init__.py
+++ src/figmentator/__init__.py
@@ -1,6 +1,7 @@
 """
 Main entry point for figmentator. This is where we setup the app.
 """
+import logging
 import os
 import urllib
 from typing import Any, Dict
@@ -8,15 +9,20 @@ from typing import Any, Dict
 import aiocache
 from fastapi import FastAPI
 
-from figmentator.routers import story, figment
-from figmentator.utils.settings import Settings
 from figmentator.figment.scheduler import Figmentators
-
+from figmentator.routers import figment, story
+from figmentator.utils.settings import Settings
 
 app = FastAPI(debug=bool(int(os.environ.get("DEBUG", 0))))
 
 app.include_router(story.router, prefix="/story", tags=["story"])
 app.include_router(figment.router, prefix="/figment", tags=["figment"])
+
+
+@app.on_event("startup")
+def initialize_logging():
+    """ Initialize the log level of the root logger """
+    logging.getLogger().setLevel(os.environ.get("LOGLEVEL", "info").upper())
 
 
 @app.on_event("startup")
